f9e3cbbe4e110683b23a51a6bf20c699d8a78621
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-05-03 09:41:54 +0100

    Add placeholder tests for matching module

---
+++ tests/test_matching.py
@@ -0,0 +1,18 @@
+import pytest
+
+from acbm.matching import match_categorical, match_individuals, match_psm  # noqa: F401
+
+
+@pytest.mark.skip(reason="todo")
+def test_match_categorical():
+    pass
+
+
+@pytest.mark.skip(reason="todo")
+def test_match_individuals():
+    pass
+
+
+@pytest.mark.skip(reason="todo")
+def test_match_psm():
+    pass
