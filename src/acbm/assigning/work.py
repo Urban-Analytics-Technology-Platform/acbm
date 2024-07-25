b153012b04593ccf083c1d5b6b1a8e74c3809cb5
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-07-25 16:28:29 +0100

    fix: optimization problem now adding variables for all activities

--- src/acbm/assigning/work.py
+++ src/acbm/assigning/work.py
@@ -3,6 +3,7 @@ import random
 from dataclasses import dataclass, field
 from typing import Dict, List, Tuple
 
+import numpy as np
 import pandas as pd
 import pulp
 
@@ -216,11 +217,160 @@ class WorkZoneAssignment:
         logger.info("Iterative assignment process completed.")
         return pd.DataFrame(assignments)
 
+    # def select_work_zone_optimization(
+    #     self,
+    #     use_percentages: bool = False,
+    #     weight_max_dev: int = 0.5,
+    #     weight_total_dev: int = 0.5,
+    # ) -> pd.DataFrame:
+    #     """
+    #     Assigns individuals to zones while minimizing the deviation between the assigned and actual flows.
+    #     We minimize two deviations
+    #     1. The sum of the deviations between the assigned and actual flows for each origin-destination pair.
+    #     2. The maximum deviation across all origin-destination pairs.
+
+    #     If we use (1) only, positive and negative deviations could cancel each other out, and
+    #     the results would not match the actual flows even though the sum of deviations is minimized.
+
+    #     The assigned flows are constrained to match the actual flows as closely as possible, either in terms
+    #     of absolute numbers or percentages of the total flows from each origin zone.
+
+    #     The function returns a DataFrame with the following columns:
+
+    #     - 'person_id': the ID of the individual
+    #     - 'origin_zone': the origin zone of the individual
+    #     - 'assigned_zone': the zone to which the individual was assigned
+
+    #     Parameters
+    #     ----------
+
+    #     activities_to_assign : dict
+    #         A dictionary where the keys are the person/activity IDs and the values are dictionaries
+    #         where keys are the origin zones and the values are the feasible destination zones.
+    #         example: {164: {'E00059011': ['E00056917','E00056922', 'E00056923']},
+    #                   165: {'E00059012': ['E00056918','E00056952', 'E00056923']}}
+
+    #     actual_flows : dict
+    #         A dictionary where the keys are the origin-destination zone pairs and the values are the
+    #         number of flows between the origin and destination zones. The intended use case is the
+    #         UK census flow data.
+    #         example: {('E00059011', 'E00056917'): 10,
+    #                   ('E00059011', 'E00056922'): 5}
+
+    #     use_percentages : bool, optional
+    #         If True, the optimization problem will minimize the deviation between the assigned and actual
+    #         flows as percentages of the total flows from each origin zone. If False, the deviation will be
+    #         minimized in terms of the absolute number of flows. Default is False.
+
+    #     weight_max_dev : int, optional
+    #         The weight assigned to the maximum deviation in the objective function. Default is 0.5.
+
+    #     weight_total_dev : int, optional
+    #         The weight assigned to the total deviation in the objective function. Default is
+
+    #     Returns
+    #     -------
+
+    #     pd.DataFrame
+    #         A DataFrame containing the assigned zones for each individual. The columns are 'person_id',
+    #         'origin_zone', and 'assigned_zone
+    #     """
+    #     # Step 1: Compute total flows per origin
+    #     # Step 2: Calculate percentages (normalize over total flow)
+
+    #     # Step 1 and 2 are already done in the __post_init__ method
+
+    #     # Step 3: Initialize the optimization problem
+    #     logger.info("Starting the optimization assignment process.")
+    #     prob = pulp.LpProblem("ZoneAssignment", pulp.LpMinimize)
+    #     # Create dictionaries to store the assignment and deviation variables
+    #     assignment_vars = {}
+    #     deviation_vars = {}
+
+    #     # Variable to track the maximum deviation across all OD pairs
+    #     max_dev = pulp.LpVariable("max_dev", 0, None, pulp.LpContinuous)
+
+    #     # Create binary variables for each person, origin, and destination
+    #     for person_id, origins in self.activities_to_assign.items():
+    #         for origin_id, feasible_zones in origins.items():
+    #             person_vars = []
+    #             for zone in feasible_zones:
+    #                 if (origin_id, zone) in self.percentages:
+    #                     var = pulp.LpVariable(
+    #                         f"assign_{person_id}_{origin_id}_{zone}",
+    #                         0,
+    #                         1,
+    #                         pulp.LpBinary,
+    #                     )
+    #                     person_vars.append(var)
+    #                     assignment_vars[(person_id, origin_id, zone)] = var
+    #             # Constraint: Assign each person to exactly one zone
+    #             if person_vars:
+    #                 prob += pulp.lpSum(person_vars) == 1
+    #     # Calculate assigned percentages and deviation for each origin-destination pair
+    #     for (from_zone, to_zone), percentage in self.percentages.items():
+    #         # Create a variable for the absolute deviation (with lower bound of 0)
+    #         abs_dev = pulp.LpVariable(
+    #             f"abs_dev_{from_zone}_{to_zone}", 0, None, pulp.LpContinuous
+    #         )
+    #         deviation_vars[(from_zone, to_zone)] = abs_dev
+    #         # Calculate the assigned flow for the origin-destination pair
+    #         assigned_flow = pulp.lpSum(
+    #             assignment_vars.get((person_id, from_zone, to_zone), 0)
+    #             for person_id, origins in self.activities_to_assign.items()
+    #             if (person_id, from_zone, to_zone) in assignment_vars
+    #         )
+    #         # Calculate the assigned percentage based on the total flows from the origin zone
+    #         if from_zone in self.total_flows:
+    #             total_people = self.total_flows[from_zone]
+    #             assigned_percentage = assigned_flow / total_people
+    #         # If the origin zone is not in the total flows, set the assigned percentage to 0
+    #         else:
+    #             assigned_percentage = 0
+    #             logger.warning(f"Warning: Origin {from_zone} not found in total_flows.")
+
+    #         if use_percentages:
+    #             # to satisfy both constraints, abs_dev will be a positive number (it is the larger of the two)
+    #             prob += assigned_percentage - percentage <= abs_dev
+    #             prob += percentage - assigned_percentage <= abs_dev
+    #         else:
+    #             prob += (
+    #                 assigned_flow - self.actual_flows[(from_zone, to_zone)] <= abs_dev
+    #             )
+    #             prob += (
+    #                 self.actual_flows[(from_zone, to_zone)] - assigned_flow <= abs_dev
+    #             )
+
+    #         # Update the maximum deviation variable: it is the maximum of all deviations
+    #         prob += max_dev >= abs_dev
+
+    #     # Weighted objective function
+    #     prob += (
+    #         weight_max_dev * max_dev
+    #         + weight_total_dev * pulp.lpSum(deviation_vars.values()),
+    #         "WeightedObjective",
+    #     )
+    #     prob.solve()
+
+    #     if pulp.LpStatus[prob.status] != "Optimal":
+    #         logger.warning("Problem is infeasible.")
+    #         return pd.DataFrame()
+
+    #     assignments = [
+    #         {"person_id": person_id, "origin_zone": origin_id, "assigned_zone": zone}
+    #         for (person_id, origin_id, zone), var in assignment_vars.items()
+    #         if var.varValue == 1
+    #     ]
+
+    #     logger.info("Optimization assignment process completed.")
+    #     return pd.DataFrame(assignments)
+
     def select_work_zone_optimization(
         self,
         use_percentages: bool = False,
         weight_max_dev: int = 0.5,
         weight_total_dev: int = 0.5,
+        max_zones: int = 10,
     ) -> pd.DataFrame:
         """
         Assigns individuals to zones while minimizing the deviation between the assigned and actual flows.
@@ -237,7 +387,7 @@ class WorkZoneAssignment:
         The function returns a DataFrame with the following columns:
 
         - 'person_id': the ID of the individual
-        - 'home_zone': the origin zone of the individual
+        - 'origin_zone': the origin zone of the individual
         - 'assigned_zone': the zone to which the individual was assigned
 
         Parameters
@@ -247,14 +397,14 @@ class WorkZoneAssignment:
             A dictionary where the keys are the person/activity IDs and the values are dictionaries
             where keys are the origin zones and the values are the feasible destination zones.
             example: {164: {'E00059011': ['E00056917','E00056922', 'E00056923']},
-                      165: {'E00059012': ['E00056918','E00056952', 'E00056923']}}
+                    165: {'E00059012': ['E00056918','E00056952', 'E00056923']}}
 
         actual_flows : dict
             A dictionary where the keys are the origin-destination zone pairs and the values are the
             number of flows between the origin and destination zones. The intended use case is the
             UK census flow data.
             example: {('E00059011', 'E00056917'): 10,
-                      ('E00059011', 'E00056922'): 5}
+                    ('E00059011', 'E00056922'): 5}
 
         use_percentages : bool, optional
             If True, the optimization problem will minimize the deviation between the assigned and actual
@@ -265,14 +415,19 @@ class WorkZoneAssignment:
             The weight assigned to the maximum deviation in the objective function. Default is 0.5.
 
         weight_total_dev : int, optional
-            The weight assigned to the total deviation in the objective function. Default is
+            The weight assigned to the total deviation in the objective function. Default is 0.5.
+
+        max_zones : int, optional
+            The maximum number of zones to consider for each person. Default is 10. For some activites,
+            we can have a large number of zones to consider. To limit the number of variables in the
+            optimization problem, we select a subset of zones based on the actual flows.
 
         Returns
         -------
 
         pd.DataFrame
             A DataFrame containing the assigned zones for each individual. The columns are 'person_id',
-            'home_zone', and 'assigned_zone
+            'origin_zone', and 'assigned_zone
         """
         # Step 1: Compute total flows per origin
         # Step 2: Calculate percentages (normalize over total flow)
@@ -293,19 +448,48 @@ class WorkZoneAssignment:
         for person_id, origins in self.activities_to_assign.items():
             for origin_id, feasible_zones in origins.items():
                 person_vars = []
-                for zone in feasible_zones:
-                    if (origin_id, zone) in self.percentages:
-                        var = pulp.LpVariable(
-                            f"assign_{person_id}_{origin_id}_{zone}",
-                            0,
-                            1,
-                            pulp.LpBinary,
-                        )
-                        person_vars.append(var)
-                        assignment_vars[(person_id, origin_id, zone)] = var
+                # Select up to 10 zones for each person based on the actual flows (to limit number of variables)
+                # Identify zones in actual_flows
+                zones_in_flows = [
+                    zone
+                    for zone in feasible_zones
+                    if (origin_id, zone) in self.actual_flows
+                ]
+
+                selected_zones = []
+                if zones_in_flows:
+                    # Extract flow values to use as weights
+                    weights = [
+                        self.actual_flows[(origin_id, zone)] for zone in zones_in_flows
+                    ]
+                    # Select up to 10 zones based on their flow values
+                    selected_zones_indices = np.random.choice(
+                        range(len(zones_in_flows)),
+                        size=min(max_zones, len(zones_in_flows)),
+                        replace=False,
+                        p=weights / np.sum(weights),  # Normalize weights to sum to 1
+                    )
+                    selected_zones = [zones_in_flows[i] for i in selected_zones_indices]
+                elif feasible_zones:
+                    # If no zones in actual_flows, select up to 10 random zones from feasible zones
+                    selected_zones = random.sample(
+                        feasible_zones, min(max_zones, len(feasible_zones))
+                    )
+
+                for zone in selected_zones:
+                    var = pulp.LpVariable(
+                        f"assign_{person_id}_{origin_id}_{zone}",
+                        0,
+                        1,
+                        pulp.LpBinary,
+                    )
+                    person_vars.append(var)
+                    assignment_vars[(person_id, origin_id, zone)] = var
+
                 # Constraint: Assign each person to exactly one zone
                 if person_vars:
                     prob += pulp.lpSum(person_vars) == 1
+
         # Calculate assigned percentages and deviation for each origin-destination pair
         for (from_zone, to_zone), percentage in self.percentages.items():
             # Create a variable for the absolute deviation (with lower bound of 0)
@@ -319,14 +503,16 @@ class WorkZoneAssignment:
                 for person_id, origins in self.activities_to_assign.items()
                 if (person_id, from_zone, to_zone) in assignment_vars
             )
-            # Calculate the assigned percentage based on the total flows from the origin zone
-            if from_zone in self.total_flows:
-                total_people = self.total_flows[from_zone]
+            # Calculate the assigned percentage based on the total flows to each destination zone
+            if to_zone in self.total_flows:
+                total_people = self.total_flows[to_zone]
                 assigned_percentage = assigned_flow / total_people
             # If the origin zone is not in the total flows, set the assigned percentage to 0
             else:
                 assigned_percentage = 0
-                logger.warning(f"Warning: Origin {from_zone} not found in total_flows.")
+                logger.warning(
+                    f"Warning: Destination {to_zone} not found in total_flows."
+                )
 
             if use_percentages:
                 # to satisfy both constraints, abs_dev will be a positive number (it is the larger of the two)
@@ -356,7 +542,7 @@ class WorkZoneAssignment:
             return pd.DataFrame()
 
         assignments = [
-            {"person_id": person_id, "home_zone": origin_id, "assigned_zone": zone}
+            {"person_id": person_id, "origin_zone": origin_id, "assigned_zone": zone}
             for (person_id, origin_id, zone), var in assignment_vars.items()
             if var.varValue == 1
         ]
