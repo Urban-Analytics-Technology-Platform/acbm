b321802e59019a56b62f3995f844fa129a65bc79
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-08-29 16:34:03 +0100

    Fix change in logger name

--- src/acbm/assigning/select_zone_secondary.py
+++ src/acbm/assigning/select_zone_secondary.py
@@ -3,9 +3,10 @@ from copy import deepcopy
 import numpy as np
 import pam
 import pandas as pd
-
 from pam.planner.choice_location import DiscretionaryTrips
-from acbm.logger_config import assigning_secondary_locations_logger as logger
+
+from acbm.logger_config import assigning_secondary_zones_logger as logger
+
 
 def set_home_ozone(data: pd.DataFrame, oact_col: str, ozone_col: str, hzone_col: str):
     """
@@ -34,7 +35,6 @@ def shift_and_fill_column(
     initial_value_col: str | None = None,
     oact_col: str | None = None,
     hzone_col: str | None = None,
-
 ) -> pd.DataFrame:
     """
     Fill the 'target_col' column by shifting the 'source_col' column within each group defined by 'group_col'.
