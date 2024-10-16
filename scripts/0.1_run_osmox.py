7e418f0a794e19bf05f4d3dd34e41d17c697f789
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-10-16 21:39:56 +0100

    Add script for osmox

---
+++ scripts/0.1_run_osmox.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+from pyrosm import get_data
+
+import acbm
+from acbm.cli import acbm_cli
+from acbm.config import load_config
+
+
+@acbm_cli
+def main(config_file):
+    config = load_config(config_file)
+    config.init_rng()
+    os.makedirs(acbm.root_path / "data/external/osmox", exist_ok=True)
+    _fp = get_data(config.region, directory=acbm.root_path / "data/external/osmox")
+    subprocess.run(
+        [
+            "osmox",
+            "run",
+            acbm.root_path / "osmox/config_osmox.json",
+            acbm.root_path / f"data/external/osmox/{config.region}.osm.pbf",
+            f"data/external/osmox/{config.region}",
+            "-f",
+            "geopackage",
+            "-crs",
+            "epsg:27700",
+            "-l",
+        ],
+        check=False,
+    )
+
+
+if __name__ == "__main__":
+    main()
