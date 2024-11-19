93f81d2b2d4549c7c7b84c8424ba0ef180465cd5
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-11-19 09:40:06 +0100

    Update scripts/run_pipeline.sh
    remove script 5
    
    Co-authored-by: Sam Greenbury <50113363+sgreenbury@users.noreply.github.com>

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -12,4 +12,3 @@ python scripts/3.2.2_assign_primary_zone_work.py --config_file $1
 python scripts/3.2.3_assign_secondary_zone.py --config_file $1
 python scripts/3.3_assign_facility_all.py --config_file $1
 python scripts/4_validation.py --config_file $1
-python scripts/5_acbm_to_matsim_xml.py --config_file $1
