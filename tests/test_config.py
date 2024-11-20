b8413ca1175469d2a551ea6a8eb36f6eff3e832f
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-11-20 12:06:43 +0000

    Remove flake8-pytest-style, add config test

---
+++ tests/test_config.py
@@ -0,0 +1,14 @@
+import pytest
+
+from acbm.config import (
+    load_config,
+)
+
+
+@pytest.fixture
+def config():
+    return load_config("config/base.toml")
+
+
+def test_id(config):
+    assert config.id == "324e59cde5"
