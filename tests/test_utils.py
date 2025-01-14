826e5ce471dce122876b2784c54abb8abab63997
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-01-14 18:13:34 +0000

    Handle missing data and revise test for common trav days

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,9 +8,26 @@ from acbm.utils import households_with_common_travel_days
 def nts_trips():
     return pd.DataFrame.from_dict(
         {
-            "IndividualID": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            "HouseholdID": [1, 1, 1, 2, 2, 2, 3, 3, 3, 3],
-            "TravDay": [1, 1, 1, 2, 3, 2, 3, 3, 3, 3],
+            "IndividualID": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            "HouseholdID": [1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5],
+            "TravDay": [
+                1,
+                1,
+                1,
+                2,
+                3,
+                2,
+                3,
+                3,
+                3,
+                3,
+                pd.NA,
+                pd.NA,
+                pd.NA,
+                pd.NA,
+                pd.NA,
+                4,
+            ],
         }
     )
 
@@ -19,3 +36,4 @@ def test_households_with_common_travel_days(nts_trips):
     assert households_with_common_travel_days(nts_trips, [1]) == [1]
     assert households_with_common_travel_days(nts_trips, [1, 2]) == [1]
     assert households_with_common_travel_days(nts_trips, [1, 3]) == [1, 3]
+    assert households_with_common_travel_days(nts_trips, [1, 3, 4]) == [1, 3]
