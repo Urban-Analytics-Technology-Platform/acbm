8e4a2887f8810657faacbde35942488cadc5caba
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-01-30 17:18:52 +0000

    Remove prepend_datetime

--- src/acbm/logger_config.py
+++ src/acbm/logger_config.py
@@ -1,11 +1,4 @@
 import logging
-from datetime import datetime
-
-
-def prepend_datetime(s: str, delimiter: str = "_") -> str:
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    return f"{current_date}{delimiter}{s}"
-
 
 # # Configure the root logger
 # logging.basicConfig(
